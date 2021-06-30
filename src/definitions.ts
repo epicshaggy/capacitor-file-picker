722411ecbc9cb7dbcfd49c4818166e1b75fda4a6
Jose Martinez <epicshaggy@gmail.com>
2021-06-30 15:05:35 -0400

    misc

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,9 +1,3 @@
-declare module "@capacitor/core" {
-  interface PluginRegistry {
-    FilePicker: FilePickerPlugin;
-  }
-}
-
 export class FiletTypes {
   static IMAGE = "image"; // For any type of image file
   static PDF = "pdf"; // For .pdf files
