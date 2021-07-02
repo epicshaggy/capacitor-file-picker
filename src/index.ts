47179f95643ff4d0f727df53be28b290ff12c63c
Jose Martinez <epicshaggy@gmail.com>
2021-07-02 14:25:31 -0400

    Added definition for FilePicker

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { registerPlugin } from "@capacitor/core";
+import { FilePickerPlugin } from "./definitions";
 
-const FilePicker = registerPlugin("FilePicker", {
+const FilePicker = registerPlugin<FilePickerPlugin>("FilePicker", {
   web: () => import("./web").then((m) => m.FilePickerWeb),
 });
 
